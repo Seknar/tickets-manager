e8090702ebcce684ded483330f21f51d4d6b8028
Pierfrancesco Sibilla <pf.sibilla@gmail.com>
2026-06-11 18:13:22 +0200

    add TicketList page

--- client/src/app/components/ticket-list/ticket-list.ts
+++ client/src/app/components/ticket-list/ticket-list.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TicketsService, Ticket } from '../../services/tickets';
 
 @Component({
   selector: 'app-ticket-list',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './ticket-list.html',
   styleUrl: './ticket-list.css',
 })
-export class TicketList {}
+export class TicketList {
+  tickets = signal<Ticket[]>([]);
+
+  constructor(private ticketsService: TicketsService) {}
+
+  ngOnInit() {
+    this.ticketsService.getTickets().subscribe({
+      next: (data) => {
+        this.tickets.set(data ?? []);
+        console.log('Tickets loaded:', this.tickets().length);
+      },
+      error: (err) => {
+          console.error('Failed to load tickets', err);
+      }
+    });
+  }
+}
--- client/src/app/services/tickets.ts
+++ client/src/app/services/tickets.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+export interface Ticket {
+  id: number;
+  title: string;
+  description: string;
+  customerName: string;
+  status: 'open' | 'in-progress' | 'closed';
+  priority: 'low' | 'medium' | 'high';
+  createdAt: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class Tickets {}
+export class TicketsService {
+  private readonly apiUrl = 'http://localhost:3000/tickets';
+
+  constructor(private http : HttpClient) {}
+
+  getTickets(): Observable<Ticket[]> {
+    return this.http.get<Ticket[]>(this.apiUrl);
+  }
+}
